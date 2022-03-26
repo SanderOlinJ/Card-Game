d70a704b958e7be38b8455ceaa5dc1cfb9f1086f
Sander Johansen <sander.olin@gmail.com>
2022-03-26 01:01:39 +0100

    Initial commit, project recreated due to JavaFX issues.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module edu.ntnu.poker_simulator_ntnu_edition {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens edu.ntnu.poker_simulator_ntnu_edition to javafx.fxml;
+    exports edu.ntnu.poker_simulator_ntnu_edition;
+}
