a18c05bad5d1281fb68186968b122319b978b598
Sander Johansen <sander.olin@gmail.com>
2022-03-26 01:14:09 +0100

    Implemented isFourOfAKind(), isThreeOfAKind() and tests

--- src/main/java/edu/ntnu/poker_simulator_ntnu_edition/Domain/HandOfCards.java
+++ src/main/java/edu/ntnu/poker_simulator_ntnu_edition/Domain/HandOfCards.java
@@ -1,6 +1,10 @@
 package edu.ntnu.poker_simulator_ntnu_edition.Domain;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class HandOfCards {
     private ArrayList<PlayingCard> hand;
@@ -18,5 +22,25 @@ public class HandOfCards {
                 hand.stream().filter(playingCard -> playingCard.getSuit() == 'H').count() > 4 ||
                 hand.stream().filter(playingCard -> playingCard.getSuit() == 'D').count() > 4 ||
                 hand.stream().filter(playingCard -> playingCard.getSuit() == 'C').count() > 4;
+    }
+
+    public boolean isFourOfAKind(){
+        return hand.stream().filter(playingCard -> playingCard.getFace() == mostOccurringFaceInHand())
+                .count() == 4;
+    }
+
+    public boolean isThreeOfAKind(){
+
+        return hand.stream().filter(playingCard -> playingCard.getFace() == mostOccurringFaceInHand())
+                .count() == 3;
+    }
+
+    public int mostOccurringFaceInHand(){
+        List<Integer> faces = hand.stream().map(playingCard -> playingCard.getFace()).collect(Collectors.toList());
+
+        return faces.stream().collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting())).entrySet()
+                .stream().max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey).orElse(0);
     }
 }
--- src/test/java/edu/ntnu/poker_simulator_ntnu_edition/Domain/HandOfCardsTest.java
+++ src/test/java/edu/ntnu/poker_simulator_ntnu_edition/Domain/HandOfCardsTest.java
@@ -47,4 +47,72 @@ class HandOfCardsTest {
             assertFalse(hand.isFlush());
         }
     }
+    @Nested
+    class testIsFourOfAKind{
+        @Test
+        @DisplayName("Test that isFourOfAKind returns true when true")
+        void testThatIsFourOfAKindReturnsTrueWhenTrue(){
+            ArrayList<PlayingCard> handOfCards = new ArrayList<>();
+
+            handOfCards.add(new PlayingCard('H',2));
+            handOfCards.add(new PlayingCard('H',2));
+            handOfCards.add(new PlayingCard('H',5));
+            handOfCards.add(new PlayingCard('H',2));
+            handOfCards.add(new PlayingCard('H',2));
+
+            handOfCards.add(new PlayingCard('D',3));
+            handOfCards.add(new PlayingCard('S',4));
+            handOfCards.add(new PlayingCard('C',5));
+            handOfCards.add(new PlayingCard('D',6));
+
+            HandOfCards hand = new HandOfCards(handOfCards);
+
+            assertTrue(hand.isFourOfAKind());
+        }
+
+        @Test
+        void testThatIsFourOfAKindReturnsFalseWhenFalse(){
+            ArrayList<PlayingCard> handOfCards = new ArrayList<>();
+            for (int i = 1; i < 6; i++){
+                handOfCards.add(new PlayingCard('S',i));
+            }
+            HandOfCards hand = new HandOfCards(handOfCards);
+
+            assertFalse(hand.isFourOfAKind());
+        }
+    }
+
+    @Nested
+    class testIsThreeOfAKind{
+        @Test
+        @DisplayName("Test that isThreeOfAKind returns true when true")
+        void testThatIsThreeOfAKindReturnsTrueWhenTrue(){
+            ArrayList<PlayingCard> handOfCards = new ArrayList<>();
+
+            handOfCards.add(new PlayingCard('H',2));
+            handOfCards.add(new PlayingCard('H',5));
+            handOfCards.add(new PlayingCard('H',2));
+            handOfCards.add(new PlayingCard('H',2));
+
+            handOfCards.add(new PlayingCard('D',3));
+            handOfCards.add(new PlayingCard('S',4));
+            handOfCards.add(new PlayingCard('C',5));
+            handOfCards.add(new PlayingCard('D',6));
+
+            HandOfCards hand = new HandOfCards(handOfCards);
+
+            assertTrue(hand.isThreeOfAKind());
+        }
+
+        @Test
+        void testThatIsThreeOfAKindReturnsFalseWhenFalse(){
+            ArrayList<PlayingCard> handOfCards = new ArrayList<>();
+            for (int i = 1; i < 6; i++){
+                handOfCards.add(new PlayingCard('S',i));
+            }
+            HandOfCards hand = new HandOfCards(handOfCards);
+
+            assertFalse(hand.isThreeOfAKind());
+        }
+    }
 }
